1d2581f4de781b59a45b48eda2a1f28c3e37564e
Buro Ibrahim <buroelias4@gmail.com>
2022-06-05 11:41:09 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- *  * main - Entry Point
+ *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
  *     */
-
 int main(void)
 {
-	char str1[70];
-
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-
-	puts(str1);
-
-
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
