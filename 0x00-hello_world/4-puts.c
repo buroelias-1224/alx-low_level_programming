818a193bbc48087cd5e605f8f7998b470be222d4
buroelias-1224 <104757613+buroelias-1224@users.noreply.github.com>
2022-06-06 16:43:29 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-int main(){
-    puts("Programming is like building a multilingual puzzle\n");
-    
-    return 0;
-}
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: print string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char My_msg[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(My_msg);
+	return (0);
