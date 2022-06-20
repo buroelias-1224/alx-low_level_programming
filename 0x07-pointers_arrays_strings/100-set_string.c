acd5a7b2ee2ee9368c694165d4ac061a11729b0f
buroelias-1224 <104757613+buroelias-1224@users.noreply.github.com>
2022-06-20 11:49:41 +0300

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+/*
+ * File: 100-set_string.c
+ * Auth: Buro Elias
+ */
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
