7d9f6789b57e8d8bd8b0b41937cf77ff74613750
buroelias-1224 <104757613+buroelias-1224@users.noreply.github.com>
2022-06-20 11:43:54 +0300

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_chessboard.c
+ * Auth: Buro Elias
+ */
+#include "main.h"
+/**
+ * print_chessboard - print a chessboard.
+ * @a: source matrix
+ *
+ *
+ * Return: chessboard.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
